af61329222181a16ca4555d3a4888f4bc0f77cb5
LonelyPyxel <lonelypyxel@yandex.com>
2016-11-09 22:23:23 -0800

    README update and rm scripts

--- scripts/lemonbar/pyxel.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/zsh
-pidfile='/tmp/pidfile'
-if [ -s "$pidfile" ]; then
-	read -r pid < "$pidfile"
-	printf "script is already running with pid; %s\nKilling...\n" "$pid"
-	kill "$pid"
-	exit 1
-fi
-
--- scripts/startupscript.sh
+++
@@ -1,5 +0,0 @@
-bmpanel --theme=twily
-sleep 2;
-feh --bg-scale /home/pyxel/Pictures/Wallpapers/NJajOSn.jpg
-
-
